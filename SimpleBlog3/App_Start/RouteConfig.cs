1fb4e9858f902458214c253534b6799439555d38
PC20831\L20805 <enescitak@gmail.com>
2017-02-23 17:54:04 +0200

    Second Commit

--- SimpleBlog3/App_Start/RouteConfig.cs
+++ SimpleBlog3/App_Start/RouteConfig.cs
@@ -12,12 +12,8 @@ namespace SimpleBlog3
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" });
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" });
         }
     }
 }
---
+++ SimpleBlog3/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog3.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return Content("Admin Areas Users Controller Index Action:");
+        }
+            public ActionResult List()
+        {
+            return Content("Auth Controller List Action:");
+        }
+        public ActionResult Edit(int id=0)
+        {
+            return Content("Auth Controller Edit Action:");
+        }
+    }
+    }
